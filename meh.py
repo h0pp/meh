2efc21ec9370d6748526b0778f55825dc948276b
h0pp <drh0pp@protonmail.com>
2019-04-09 13:39:44 +0200

    Tabs converted to spaces

--- meh.py
+++ meh.py
@@ -7,52 +7,50 @@ DIR = ""
 PROJECTS = []
 
 def clear(): 
-  
-    # for windows 
-    if os.name == 'nt': 
-        _ = os.system('cls') 
-  
-    # for mac and linux(here, os.name is 'posix') 
-    else: 
-        _ = os.system('clear') 
+  # for windows 
+  if os.name == 'nt': 
+    _ = os.system('cls') 
+  # for mac and linux(here, os.name is 'posix') 
+  else: 
+    _ = os.system('clear') 
   
 def init():
-	global DIR 
-	config = configparser.ConfigParser()
-	config.read("./meh.conf")
-	DIR = config['CORE']['Directory']
+  global DIR 
+  config = configparser.ConfigParser()
+  config.read("./meh.conf")
+  DIR = config['CORE']['Directory']
 
 def read_proyects():
-	global DIR, PROJECTS
-	print(DIR)
-	PROJECTS = glob("{}/*/".format(DIR))
+  global DIR, PROJECTS
+  print(DIR)
+  PROJECTS = glob("{}/*/".format(DIR))
 
 def menu():
-	global PROJECTS
-	while True:
-		clear()
-		print("+++ Select a project to update +++")
-		i = 0
-		for p in PROJECTS:
-			print("{}. {}".format(i,p.split(os.sep)[-2]))
-			i += 1
-		print("{}. all".format(i))
-		pr = int(input("Pick one: "))
-		if(pr > 0 and pr < len(PROJECTS)):
-			print("Updating..")
-			g = git.cmd.Git(PROJECTS[pr])
-			g.pull()
-			print("Updated")
-		elif(pr == len(PROJECTS)):
-			print("Update all..")
-			for p in PROJECTS:
-				g = git.cmd.Git(p)
-				g.pull()
-			print("Updated..")
-			break
-		else:
-			print("Out of scope")
-			input("Press enter to continue")
+  global PROJECTS
+  while True:
+    clear()
+    print("+++ Select a project to update +++")
+    i = 0
+    for p in PROJECTS:
+      print("{}. {}".format(i,p.split(os.sep)[-2]))
+      i += 1
+    print("{}. all".format(i))
+    pr = int(input("Pick one: "))
+    if(pr > 0 and pr < len(PROJECTS)):
+      print("Updating..")
+      g = git.cmd.Git(PROJECTS[pr])
+      g.pull()
+      print("Updated")
+    elif(pr == len(PROJECTS)):
+      print("Update all..")
+      for p in PROJECTS:
+        g = git.cmd.Git(p)
+        g.pull()
+      print("Updated..")
+      break
+    else:
+      print("Out of scope")
+      input("Press enter to continue")
 
 
 init()
